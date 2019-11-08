fdceb54872bc0e8c0af499c152d244a757e53523
Raihan <raihanraju02@gmail.com>
2019-11-08 10:59:26 +0600

    Animation

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -10,7 +10,7 @@ func delay(_ seconds: Double, completion: @escaping ()->Void) {
 class ViewController: UIViewController {
   
   // MARK: IB outlets
-  
+  private var timer: Timer?
   @IBOutlet var loginButton: UIButton!
   @IBOutlet var heading: UILabel!
   @IBOutlet var username: UITextField!
@@ -20,8 +20,17 @@ class ViewController: UIViewController {
   @IBOutlet var cloud2: UIImageView!
   @IBOutlet var cloud3: UIImageView!
   @IBOutlet var cloud4: UIImageView!
+  @IBOutlet weak var cloudOneCenterX: NSLayoutConstraint!
+  @IBOutlet weak var cloudTwoCenterX: NSLayoutConstraint!
+  @IBOutlet weak var cloudeThreeHeight: NSLayoutConstraint!
+  @IBOutlet weak var cloudThreeWidth: NSLayoutConstraint!
   
-  // MARK: further UI
+  @IBOutlet weak var cloudFourHieght: NSLayoutConstraint!
+    
+  @IBOutlet weak var cloudFourWidth: NSLayoutConstraint!
+    
+    
+    // MARK: further UI
   
   let spinner = UIActivityIndicatorView(style: .whiteLarge)
   let status = UIImageView(image: UIImage(named: "banner"))
@@ -61,7 +70,33 @@ class ViewController: UIViewController {
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
+    let timer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(showView), userInfo: nil, repeats: true)
+    self.timer = timer
   }
+    
+    @objc func showView(){
+      cloudOneCenterX.constant = self.view.frame.width
+      cloudTwoCenterX.constant = -self.view.frame.width
+        cloudThreeWidth.constant = 2 * cloudThreeWidth.constant
+      cloudeThreeHeight.constant = 2 * cloudeThreeHeight.constant
+      cloudFourWidth.constant = 2 * cloudFourWidth.constant
+      cloudFourHieght.constant = 2*cloudFourHieght.constant
+      UIView.animate(withDuration: 3, delay: 0, animations: {
+            self.view.layoutIfNeeded()
+        }) { (_) in
+          self.cloudOneCenterX.constant = -self.view.frame.width
+          self.cloudTwoCenterX.constant = self.view.frame.width + self.cloud2.frame.width
+          
+          
+        }
+      self.cloudFourHieght.constant = self.cloudFourHieght.constant / 2
+      self.cloudFourWidth.constant = self.cloudFourWidth.constant / 2
+      self.cloudThreeWidth.constant = self.cloudThreeWidth.constant / 2
+      self.cloudeThreeHeight.constant = self.cloudeThreeHeight.constant / 2
+      UIView.animate(withDuration: 2, delay: 3, animations: {
+          self.view.layoutIfNeeded()
+      })
+    }
   
   // MARK: further methods
   
